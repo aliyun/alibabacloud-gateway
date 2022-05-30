0ffd27c3d38e33c7b141026f57608ba033b5696f
yndu13 <yndu13@gmail.com>
2022-05-30 11:59:21 +0800

    [FC util]support python2

---
+++ alibabacloud-gateway-fc/util/python2/alibabacloud_gateway_fc_util/client.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+# This file is auto-generated, don't edit it. Thanks.
+from requests import Request
+
+
+class Client(object):
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def invoke_httptrigger(credential, url, method, body, headers):
+        raise Exception('Un-implemented')
+
+    @staticmethod
+    def invoke_anonymous_httptrigger(url, method, body, headers):
+        raise Exception('Un-implemented')
+
+    @staticmethod
+    def send_httprequest_with_authorization(credential, req):
+        raise Exception('Un-implemented')
+
+    @staticmethod
+    def send_httprequest(req):
+        raise Exception('Un-implemented')
+
+    @staticmethod
+    def sign_request(credential, req):
+        raise Exception('Un-implemented')
+
+    @staticmethod
+    def sign_request_with_content_md5(credential, req, content_md5):
+        raise Exception('Un-implemented')
+
+    @staticmethod
+    def build_httprequest(url, method, body, headers):
+        raise Exception('Un-implemented')
