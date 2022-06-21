857fa18a6f433b4e013ac5ca651365175216fc42
yndu13 <yndu13@gmail.com>
2022-06-21 10:33:39 +0800

    [PDS gateway]Bump 0.0.1

---
+++ alibabacloud-gateway-pds/python2/alibabacloud_gateway_pds/client.py
@@ -0,0 +1,175 @@
+# -*- coding: utf-8 -*-
+# This file is auto-generated, don't edit it. Thanks.
+from __future__ import unicode_literals
+
+from Tea.exceptions import TeaException
+from alibabacloud_darabonba_signature_util.signer import Signer
+from alibabacloud_darabonba_encode_util.encoder import Encoder
+from Tea.converter import TeaConverter
+from Tea.core import TeaCore
+
+from alibabacloud_gateway_spi.client import Client as SPIClient
+from alibabacloud_tea_util.client import Client as UtilClient
+from alibabacloud_openapi_util.client import Client as OpenApiUtilClient
+from alibabacloud_darabonba_string.client import Client as StringClient
+from alibabacloud_darabonba_map.client import Client as MapClient
+from alibabacloud_darabonba_array.client import Client as ArrayClient
+
+
+class Client(SPIClient):
+    def __init__(self):
+        super(Client, self).__init__()
+
+    def modify_configuration(self, context, attribute_map):
+        config = context.configuration
+        config.endpoint = self.get_endpoint(config.network, config.endpoint)
+
+    def modify_request(self, context, attribute_map):
+        request = context.request
+        config = context.configuration
+        host_map = {}
+        if not UtilClient.is_unset(request.host_map):
+            host_map = request.host_map
+        domain_id = host_map.get('domain_id')
+        if UtilClient.is_unset(domain_id):
+            domain_id = ''
+        host = '%s.%s' % (TeaConverter.to_unicode(domain_id), TeaConverter.to_unicode(config.endpoint))
+        request.headers = TeaCore.merge({
+            'date': UtilClient.get_date_utcstring(),
+            'host': host,
+            'x-acs-version': request.version,
+            'x-acs-action': request.action,
+            'user-agent': request.user_agent,
+            'x-acs-signature-nonce': UtilClient.get_nonce(),
+            'x-acs-signature-method': 'HMAC-SHA1',
+            'x-acs-signature-version': '1.0',
+            'accept': 'application/json'
+        }, request.headers)
+        if not UtilClient.is_unset(request.stream):
+            tmp = UtilClient.read_as_bytes(request.stream)
+            request.stream = tmp
+            request.headers['content-type'] = 'application/octet-stream'
+        else:
+            if not UtilClient.is_unset(request.body):
+                if UtilClient.equal_string(request.req_body_type, 'json'):
+                    json_obj = UtilClient.to_jsonstring(request.body)
+                    request.stream = json_obj
+                    request.headers['content-type'] = 'application/json; charset=utf-8'
+                else:
+                    m = UtilClient.assert_as_map(request.body)
+                    form_obj = OpenApiUtilClient.to_form(m)
+                    request.stream = form_obj
+                    request.headers['content-type'] = 'application/x-www-form-urlencoded'
+        if not UtilClient.equal_string(request.auth_type, 'Anonymous'):
+            credential = request.credential
+            access_key_id = credential.get_access_key_id()
+            access_key_secret = credential.get_access_key_secret()
+            security_token = credential.get_security_token()
+            bearer_token = credential.get_bearer_token()
+            if not UtilClient.empty(bearer_token):
+                request.headers['x-acs-bearer-token'] = bearer_token
+                request.headers['Authorization'] = 'Bearer %s' % TeaConverter.to_unicode(bearer_token)
+            else:
+                if not UtilClient.empty(security_token):
+                    request.headers['x-acs-security-token'] = security_token
+                request.headers['Authorization'] = self.get_authorization(request.pathname, request.method, request.query, request.headers, access_key_id, access_key_secret)
+
+    def modify_response(self, context, attribute_map):
+        request = context.request
+        response = context.response
+        if UtilClient.is_4xx(response.status_code) or UtilClient.is_5xx(response.status_code):
+            _res = UtilClient.read_as_json(response.body)
+            err = UtilClient.assert_as_map(_res)
+            request_id = self.default_any(err.get('RequestId'), err.get('requestId'))
+            err['statusCode'] = response.status_code
+            raise TeaException({
+                'code': '%s' % TeaConverter.to_unicode(self.default_any(err.get('Code'), err.get('code'))),
+                'message': 'code: %s, %s request id: %s' % (TeaConverter.to_unicode(response.status_code), TeaConverter.to_unicode(self.default_any(err.get('Message'), err.get('message'))), TeaConverter.to_unicode(request_id)),
+                'data': err
+            })
+        if UtilClient.equal_string(request.body_type, 'binary'):
+            response.deserialized_body = response.body
+        elif UtilClient.equal_string(request.body_type, 'byte'):
+            byt = UtilClient.read_as_bytes(response.body)
+            response.deserialized_body = byt
+        elif UtilClient.equal_string(request.body_type, 'string'):
+            str = UtilClient.read_as_string(response.body)
+            response.deserialized_body = str
+        elif UtilClient.equal_string(request.body_type, 'json'):
+            response.deserialized_body = UtilClient.read_as_json(response.body)
+        else:
+            response.deserialized_body = UtilClient.read_as_string(response.body)
+
+    def get_endpoint(self, network, endpoint):
+        real_endpoint = 'api.aliyunpds.com'
+        if not UtilClient.empty(endpoint):
+            real_endpoint = endpoint
+        if not UtilClient.empty(network) and StringClient.equals(network, 'vpc'):
+            url = StringClient.split(real_endpoint, '.', 0)
+            tmp = url[0]
+            real_endpoint = '%s-vpc.aliyunpds.com' % TeaConverter.to_unicode(tmp)
+        return real_endpoint
+
+    def default_any(self, input_value, default_value):
+        if UtilClient.is_unset(input_value):
+            return default_value
+        return input_value
+
+    def get_authorization(self, pathname, method, query, headers, ak, secret):
+        signature = self.get_signature(pathname, method, query, headers, secret)
+        return 'acs %s:%s' % (TeaConverter.to_unicode(ak), TeaConverter.to_unicode(signature))
+
+    def get_signature(self, pathname, method, query, headers, secret):
+        string_to_sign = ''
+        canonicalized_resource = self.build_canonicalized_resource(pathname, query)
+        canonicalized_headers = self.build_canonicalized_headers(headers)
+        string_to_sign = '%s\n%s%s' % (TeaConverter.to_unicode(method), TeaConverter.to_unicode(canonicalized_headers), TeaConverter.to_unicode(canonicalized_resource))
+        signature = Signer.hmac_sha1sign(string_to_sign, secret)
+        return Encoder.base_64encode_to_string(signature)
+
+    def build_canonicalized_resource(self, pathname, query):
+        canonicalized_resource = pathname
+        if not UtilClient.is_unset(query):
+            query_array = MapClient.key_set(query)
+            sorted_query_array = ArrayClient.asc_sort(query_array)
+            separator = '?'
+            for key in sorted_query_array:
+                canonicalized_resource = '%s%s%s' % (TeaConverter.to_unicode(canonicalized_resource), TeaConverter.to_unicode(separator), TeaConverter.to_unicode(key))
+                if not UtilClient.empty(query.get(key)):
+                    canonicalized_resource = '%s=%s' % (TeaConverter.to_unicode(canonicalized_resource), TeaConverter.to_unicode(query.get(key)))
+                separator = '&'
+        return canonicalized_resource
+
+    def build_canonicalized_headers(self, headers):
+        accept = headers.get('accept')
+        if UtilClient.is_unset(accept):
+            accept = ''
+        content_md_5 = headers.get('content-md5')
+        if UtilClient.is_unset(content_md_5):
+            content_md_5 = ''
+        content_type = headers.get('content-type')
+        if UtilClient.is_unset(content_type):
+            content_type = ''
+        date = headers.get('date')
+        if UtilClient.is_unset(date):
+            date = ''
+        canonicalized_headers = '%s\n%s\n%s\n%s\n' % (TeaConverter.to_unicode(accept), TeaConverter.to_unicode(content_md_5), TeaConverter.to_unicode(content_type), TeaConverter.to_unicode(date))
+        sorted_headers = self.get_signed_headers(headers)
+        for header in sorted_headers:
+            value = headers.get(header)
+            value_trim = StringClient.trim(value)
+            canonicalized_headers = '%s%s:%s\n' % (TeaConverter.to_unicode(canonicalized_headers), TeaConverter.to_unicode(header), TeaConverter.to_unicode(value_trim))
+        return canonicalized_headers
+
+    def get_signed_headers(self, headers):
+        headers_array = MapClient.key_set(headers)
+        sorted_headers_array = ArrayClient.asc_sort(headers_array)
+        tmp = ''
+        separator = ''
+        for key in sorted_headers_array:
+            lower_key = StringClient.to_lower(key)
+            if StringClient.has_prefix(lower_key, 'x-acs-'):
+                if not StringClient.contains(tmp, lower_key):
+                    tmp = '%s%s%s' % (TeaConverter.to_unicode(tmp), TeaConverter.to_unicode(separator), TeaConverter.to_unicode(lower_key))
+                    separator = ';'
+        return StringClient.split(tmp, ';', 0)
---
+++ alibabacloud-gateway-pds/ts/src/client.ts
@@ -0,0 +1,234 @@
+// This file is auto-generated, don't edit it
+import SPI, * as $SPI from '@alicloud/gateway-spi';
+import Credential from '@alicloud/credentials';
+import Util from '@alicloud/tea-util';
+import OpenApiUtil from '@alicloud/openapi-util';
+import EncodeUtil from '@alicloud/darabonba-encode-util';
+import SignatureUtil from '@alicloud/darabonba-signature-util';
+import String from '@alicloud/darabonba-string';
+import Map from '@alicloud/darabonba-map';
+import Array from '@alicloud/darabonba-array';
+import * as $tea from '@alicloud/tea-typescript';
+
+
+export default class Client extends SPI {
+
+  constructor() {
+    super();
+  }
+
+
+  async modifyConfiguration(context: $SPI.InterceptorContext, attributeMap: $SPI.AttributeMap): Promise<void> {
+    let config = context.configuration;
+    config.endpoint = await this.getEndpoint(config.network, config.endpoint);
+  }
+
+  async modifyRequest(context: $SPI.InterceptorContext, attributeMap: $SPI.AttributeMap): Promise<void> {
+    let request = context.request;
+    let config = context.configuration;
+    let hostMap : {[key: string ]: string} = { };
+    if (!Util.isUnset(request.hostMap)) {
+      hostMap = request.hostMap;
+    }
+
+    let domainId = hostMap["domain_id"];
+    if (Util.isUnset(domainId)) {
+      domainId = "";
+    }
+
+    let host = `${domainId}.${config.endpoint}`;
+    request.headers = {
+      date: Util.getDateUTCString(),
+      host: host,
+      'x-acs-version': request.version,
+      'x-acs-action': request.action,
+      'user-agent': request.userAgent,
+      'x-acs-signature-nonce': Util.getNonce(),
+      'x-acs-signature-method': "HMAC-SHA1",
+      'x-acs-signature-version': "1.0",
+      accept: "application/json",
+      ...request.headers,
+    };
+    if (!Util.isUnset(request.stream)) {
+      let tmp = await Util.readAsBytes(request.stream);
+      request.stream = new $tea.BytesReadable(tmp);
+      request.headers["content-type"] = "application/octet-stream";
+    } else {
+      if (!Util.isUnset(request.body)) {
+        if (Util.equalString(request.reqBodyType, "json")) {
+          let jsonObj = Util.toJSONString(request.body);
+          request.stream = new $tea.BytesReadable(jsonObj);
+          request.headers["content-type"] = "application/json; charset=utf-8";
+        } else {
+          let m = Util.assertAsMap(request.body);
+          let formObj = OpenApiUtil.toForm(m);
+          request.stream = new $tea.BytesReadable(formObj);
+          request.headers["content-type"] = "application/x-www-form-urlencoded";
+        }
+
+      }
+
+    }
+
+    if (!Util.equalString(request.authType, "Anonymous")) {
+      let credential : Credential = request.credential;
+      let accessKeyId = await credential.getAccessKeyId();
+      let accessKeySecret = await credential.getAccessKeySecret();
+      let securityToken = await credential.getSecurityToken();
+      let bearerToken = credential.getBearerToken();
+      if (!Util.empty(bearerToken)) {
+        request.headers["x-acs-bearer-token"] = bearerToken;
+        request.headers["Authorization"] = `Bearer ${bearerToken}`;
+      } else {
+        if (!Util.empty(securityToken)) {
+          request.headers["x-acs-security-token"] = securityToken;
+        }
+
+        request.headers["Authorization"] = await this.getAuthorization(request.pathname, request.method, request.query, request.headers, accessKeyId, accessKeySecret);
+      }
+
+    }
+
+  }
+
+  async modifyResponse(context: $SPI.InterceptorContext, attributeMap: $SPI.AttributeMap): Promise<void> {
+    let request = context.request;
+    let response = context.response;
+    if (Util.is4xx(response.statusCode) || Util.is5xx(response.statusCode)) {
+      let _res = await Util.readAsJSON(response.body);
+      let err = Util.assertAsMap(_res);
+      let requestId = this.defaultAny(err["RequestId"], err["requestId"]);
+      err["statusCode"] = response.statusCode;
+      throw $tea.newError({
+        code: `${this.defaultAny(err["Code"], err["code"])}`,
+        message: `code: ${response.statusCode}, ${this.defaultAny(err["Message"], err["message"])} request id: ${requestId}`,
+        data: err,
+      });
+    }
+
+    if (Util.equalString(request.bodyType, "binary")) {
+      response.deserializedBody = response.body;
+    } else if (Util.equalString(request.bodyType, "byte")) {
+      let byt = await Util.readAsBytes(response.body);
+      response.deserializedBody = byt;
+    } else if (Util.equalString(request.bodyType, "string")) {
+      let str = await Util.readAsString(response.body);
+      response.deserializedBody = str;
+    } else if (Util.equalString(request.bodyType, "json")) {
+      response.deserializedBody = await Util.readAsJSON(response.body);
+    } else {
+      response.deserializedBody = await Util.readAsString(response.body);
+    }
+
+  }
+
+  async getEndpoint(network: string, endpoint: string): Promise<string> {
+    let realEndpoint = "api.aliyunpds.com";
+    if (!Util.empty(endpoint)) {
+      realEndpoint = endpoint;
+    }
+
+    if (!Util.empty(network) && String.equals(network, "vpc")) {
+      let url : string[] = String.split(realEndpoint, ".", 0);
+      let tmp = url[0];
+      realEndpoint = `${tmp}-vpc.aliyunpds.com`;
+    }
+
+    return realEndpoint;
+  }
+
+  defaultAny(inputValue: any, defaultValue: any): any {
+    if (Util.isUnset(inputValue)) {
+      return defaultValue;
+    }
+
+    return inputValue;
+  }
+
+  async getAuthorization(pathname: string, method: string, query: {[key: string ]: string}, headers: {[key: string ]: string}, ak: string, secret: string): Promise<string> {
+    let signature = await this.getSignature(pathname, method, query, headers, secret);
+    return `acs ${ak}:${signature}`;
+  }
+
+  async getSignature(pathname: string, method: string, query: {[key: string ]: string}, headers: {[key: string ]: string}, secret: string): Promise<string> {
+    let stringToSign : string = "";
+    let canonicalizedResource = await this.buildCanonicalizedResource(pathname, query);
+    let canonicalizedHeaders = await this.buildCanonicalizedHeaders(headers);
+    stringToSign = `${method}\n${canonicalizedHeaders}${canonicalizedResource}`;
+    let signature = SignatureUtil.HmacSHA1Sign(stringToSign, secret);
+    return EncodeUtil.base64EncodeToString(signature);
+  }
+
+  async buildCanonicalizedResource(pathname: string, query: {[key: string ]: string}): Promise<string> {
+    let canonicalizedResource : string = pathname;
+    if (!Util.isUnset(query)) {
+      let queryArray : string[] = Map.keySet(query);
+      let sortedQueryArray = Array.ascSort(queryArray);
+      let separator : string = "?";
+
+      for (let key of sortedQueryArray) {
+        canonicalizedResource = `${canonicalizedResource}${separator}${key}`;
+        if (!Util.empty(query[key])) {
+          canonicalizedResource = `${canonicalizedResource}=${query[key]}`;
+        }
+
+        separator = "&";
+      }
+    }
+
+    return canonicalizedResource;
+  }
+
+  async buildCanonicalizedHeaders(headers: {[key: string ]: string}): Promise<string> {
+    let accept = headers["accept"];
+    if (Util.isUnset(accept)) {
+      accept = "";
+    }
+
+    let contentMd5 = headers["content-md5"];
+    if (Util.isUnset(contentMd5)) {
+      contentMd5 = "";
+    }
+
+    let contentType = headers["content-type"];
+    if (Util.isUnset(contentType)) {
+      contentType = "";
+    }
+
+    let date = headers["date"];
+    if (Util.isUnset(date)) {
+      date = "";
+    }
+
+    let canonicalizedHeaders : string = `${accept}\n${contentMd5}\n${contentType}\n${date}\n`;
+    let sortedHeaders : string[] = await this.getSignedHeaders(headers);
+
+    for (let header of sortedHeaders) {
+      let value = headers[header];
+      let valueTrim = String.trim(value);
+      canonicalizedHeaders = `${canonicalizedHeaders}${header}:${valueTrim}\n`;
+    }
+    return canonicalizedHeaders;
+  }
+
+  async getSignedHeaders(headers: {[key: string ]: string}): Promise<string[]> {
+    let headersArray : string[] = Map.keySet(headers);
+    let sortedHeadersArray = Array.ascSort(headersArray);
+    let tmp : string = "";
+    let separator : string = "";
+
+    for (let key of sortedHeadersArray) {
+      let lowerKey = String.toLower(key);
+      if (String.hasPrefix(lowerKey, "x-acs-")) {
+        if (!String.contains(tmp, lowerKey)) {
+          tmp = `${tmp}${separator}${lowerKey}`;
+          separator = ";";
+        }
+
+      }
+
+    }
+    return String.split(tmp, ";", 0);
+  }
+
+}
