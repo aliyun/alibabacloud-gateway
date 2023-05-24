5934e4c0cb7789d69a692064183a13223b8d7f45
zhurenzhu.zrz <zhurenzhu.zrz@alibaba-inc.com>
2023-05-24 11:39:22 +0800

    java support uncompress

--- alibabacloud-gateway-sls/util/java/src/main/java/com/aliyun/gateway/sls/util/Client.java
+++ alibabacloud-gateway-sls/util/java/src/main/java/com/aliyun/gateway/sls/util/Client.java
@@ -1,8 +1,20 @@
 // This file is auto-generated, don't edit it. Thanks.
 package com.aliyun.gateway.sls.util;
 
+import java.io.ByteArrayInputStream;
+
 public class Client {
 
-    public static Object readAndUncompressBlock(java.io.InputStream stream, String bodyRawSize) throws Exception {
+    public static Object readAndUncompressBlock(java.io.InputStream stream, String compressType, String bodyRawSize) throws Exception {
+        byte[] rawData = com.aliyun.teautil.Common.readAsBytes(stream);
+        int rawSize = Integer.parseInt(bodyRawSize);
+        if (compressType.equals("lz4")) {
+            rawData = LZ4Encoder.decompressFromLhLz4Chunk(rawData, rawSize);
+        } else if (compressType.equals("gzip")) {
+            rawData = GzipUtils.uncompress(rawData);
+        }
+        String data = new String(rawData, "UTF-8");
+        return new ByteArrayInputStream(data.getBytes());
     }
+
 }
