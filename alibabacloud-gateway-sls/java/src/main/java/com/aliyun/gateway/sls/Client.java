73a3e532d234c6744d1ccfbbb69a4921efada1e6
zhurenzhu.zrz <zhurenzhu.zrz@alibaba-inc.com>
2023-05-29 10:53:45 +0800

    fix

--- alibabacloud-gateway-sls/java/src/main/java/com/aliyun/gateway/sls/Client.java
+++ alibabacloud-gateway-sls/java/src/main/java/com/aliyun/gateway/sls/Client.java
@@ -73,7 +73,6 @@ public class Client extends com.aliyun.gateway.spi.Client {
     public void modifyResponse(com.aliyun.gateway.spi.models.InterceptorContext context, com.aliyun.gateway.spi.models.AttributeMap attributeMap) throws Exception {
         com.aliyun.gateway.spi.models.InterceptorContext.InterceptorContextRequest request = context.request;
         com.aliyun.gateway.spi.models.InterceptorContext.InterceptorContextResponse response = context.response;
-
         if (com.aliyun.teautil.Common.is4xx(response.statusCode) || com.aliyun.teautil.Common.is5xx(response.statusCode)) {
             Object error = com.aliyun.teautil.Common.readAsJSON(response.body);
             java.util.Map<String, Object> resMap = com.aliyun.teautil.Common.assertAsMap(error);
