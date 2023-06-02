c0a6a8bfb74f73919dc2f34fd0dafd119bb2fcd0
zhurenzhu.zrz <zhurenzhu.zrz@alibaba-inc.com>
2023-06-02 16:57:29 +0800

    [SLS gateway util]feature: go support uncompress

--- alibabacloud-gateway-sls/util/golang/client/client.go
+++ alibabacloud-gateway-sls/util/golang/client/client.go
@@ -7,9 +7,29 @@
 package client
 
 import (
+	"bytes"
+	"fmt"
+	util "github.com/alibabacloud-go/tea-utils/v2/service"
+	"github.com/pierrec/lz4"
 	"io"
+	"strconv"
 )
 
 func ReadAndUncompressBlock(stream io.Reader, compressType *string, bodyRawSize *string) (_result io.Reader, _err error) {
-	panic("No Support!")
+	if *compressType != "lz4" {
+		return nil, fmt.Errorf("unsupported compress type %s", *compressType)
+	}
+	rawSize, err := strconv.ParseInt(*bodyRawSize, 10, 64)
+	if err != nil {
+		return nil, err
+	}
+	out := make([]byte, rawSize)
+	if rawSize != 0 {
+		body, _ := util.ReadAsBytes(stream)
+		len, err := lz4.UncompressBlock(body, out)
+		if err != nil || int64(len) != rawSize {
+			return nil, err
+		}
+	}
+	return bytes.NewReader(out), nil
 }
