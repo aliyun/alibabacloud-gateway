e1f366ec39dbe42ce0349f04c6b3e4c6aac8024d
zhurenzhu.zrz <zhurenzhu.zrz@alibaba-inc.com>
2024-04-11 16:12:29 +0800

    [sls gateway tea]support c# php util

--- alibabacloud-gateway-sls/util/csharp/core/Client.cs
+++ alibabacloud-gateway-sls/util/csharp/core/Client.cs
@@ -6,11 +6,9 @@
 // This file is auto-generated, don't edit it. Thanks.
 
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
-
+using Ionic.Zlib;
 
 namespace AlibabaCloud.GatewaySls_Util
 {
@@ -19,7 +17,30 @@ namespace AlibabaCloud.GatewaySls_Util
 
         public static Stream ReadAndUncompressBlock(Stream stream, string compressType, string bodyRawSize)
         {
-            throw new NotImplementedException();
+            if (!string.Equals(compressType, "gzip", StringComparison.OrdinalIgnoreCase))
+            {
+                throw new NotSupportedException(string.Format("{0} compression type is not implemented.", compressType));
+            }
+            long expectedSize;
+            if (!long.TryParse(bodyRawSize, out expectedSize))
+            {
+                throw new ArgumentException("Invalid bodyRawSize value. It must be a valid numeric string.");
+            }
+
+            stream.Position = 0;
+            MemoryStream decompressedStream = new MemoryStream();
+            using (var decompressionStream = new ZlibStream(stream, Ionic.Zlib.CompressionMode.Decompress))
+            {
+                decompressionStream.CopyTo(decompressedStream);
+            }
+            decompressedStream.Position = 0;
+            if (decompressedStream.Length != expectedSize)
+            {
+                throw new InvalidDataException(
+                    string.Format("Decompressed data size is {0}, which does not match the expected size of {1}.",
+                    decompressedStream.Length, expectedSize));
+            }
+            return decompressedStream;
         }
 
         public static async Task<Stream> ReadAndUncompressBlockAsync(Stream stream, string compressType, string bodyRawSize)
