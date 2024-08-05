c090a88cd8be2563916afb9a77cff0c89e7fc770
山泉 <daiqianwang.dqw@alibaba-inc.com>
2024-08-05 17:21:43 +0800

    [oss gateway util] add java support for hcs-mgw, bump util v0.0.4, bump tea v0.0.18

---
+++ alibabacloud-gateway-oss/util/java/src/main/java/com/aliyun/gateway/oss/util/models/FileTypeFilters.java
@@ -0,0 +1,42 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.gateway.oss.util.models;
+
+import com.aliyun.tea.*;
+
+public class FileTypeFilters extends TeaModel {
+    /**
+     * <strong>example:</strong>
+     * <p>fasle</p>
+     */
+    @NameInMap("ExcludeDir")
+    public Boolean excludeDir;
+
+    /**
+     * <strong>example:</strong>
+     * <p>fasle</p>
+     */
+    @NameInMap("ExcludeSymlink")
+    public Boolean excludeSymlink;
+
+    public static FileTypeFilters build(java.util.Map<String, ?> map) throws Exception {
+        FileTypeFilters self = new FileTypeFilters();
+        return TeaModel.build(map, self);
+    }
+
+    public FileTypeFilters setExcludeDir(Boolean excludeDir) {
+        this.excludeDir = excludeDir;
+        return this;
+    }
+    public Boolean getExcludeDir() {
+        return this.excludeDir;
+    }
+
+    public FileTypeFilters setExcludeSymlink(Boolean excludeSymlink) {
+        this.excludeSymlink = excludeSymlink;
+        return this;
+    }
+    public Boolean getExcludeSymlink() {
+        return this.excludeSymlink;
+    }
+
+}
