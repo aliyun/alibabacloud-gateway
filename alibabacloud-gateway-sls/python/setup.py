8115c6367dc35b31d1ce0c63e5da6df62e5ed28f
shuizhao.gh <shuizhao.gh@alibaba-inc.com>
2024-09-24 10:18:04 +0800

    feat(sls): python use util version 0.0.3

--- alibabacloud-gateway-sls/python/setup.py
+++ alibabacloud-gateway-sls/python/setup.py
@@ -46,7 +46,7 @@ REQUIRES = [
     "alibabacloud_darabonba_array>=0.1.0, <1.0.0",
     "alibabacloud_darabonba_encode_util>=0.0.2, <1.0.0",
     "alibabacloud_darabonba_signature_util>=0.0.4, <1.0.0",
-    "alibabacloud_gateway_sls_util>=0.0.1, <1.0.0"
+    "alibabacloud_gateway_sls_util>=0.0.3, <1.0.0"
 ]
 
 LONG_DESCRIPTION = ''
